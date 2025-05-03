d5b8247663397de47be83797c36f3bc63b38ae4e
10thmdhll <131085434+10thmdhll@users.noreply.github.com>
2025-05-03 14:46:25 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,8 +23,8 @@ CONFIG = {
     "quantize_colors":  64,
     "compress_level":   9,
     "optimize_png":     True,
-    "font_size_h":      32,
-    "font_size":        24,
+    "font_size_h":      48,
+    "font_size":        36,
     "font_paths": [
         "arialbd.ttf",
         "DejaVuSans-Bold.ttf",
