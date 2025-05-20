ef6421718cfd41f33b2973f76c3ea269135fb469
10thmdhll <131085434+10thmdhll@users.noreply.github.com>
2025-05-20 15:47:06 -0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -95,8 +95,8 @@ async def update_host_mode_choice_embed(channel: discord.TextChannel, message_id
     await msg.edit(embed=embed)
     
     if new_choice == "host":
-        new_turn = await flip_turn(message_id)
-        await update_current_turn_embed(interaction.channel, message_id, new_turn)
+        new_turn = await flip_turn(channel_id)
+        await update_current_turn_embed(interaction.channel, msg, new_turn)
     
 async def update_ban_mode_choice_embed(channel: discord.TextChannel, message_id: int, new_choice: str):
     # 1) Fetch the bot’s original embed message
