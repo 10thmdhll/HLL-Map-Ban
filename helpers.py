1f67b868c5198686d14a58138034a93e22edba60
10thmdhll <131085434+10thmdhll@users.noreply.github.com>
2025-05-20 22:24:43 -0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -474,6 +474,7 @@ def create_combo_grid_image(
 
 async def send_remaining_maps_embed(
     interaction: discord.Interaction,
+    channel: discord.TextChannel,
     maps: List[str],
     state_data: Dict[str, Dict[str, Dict[str, List[str]]]],
     team_names: Tuple[str, str] = ("Team A", "Team B")
