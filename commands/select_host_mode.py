32188ca0455f6013fd663e8758efa0a9182c1045
10thmdhll <131085434+10thmdhll@users.noreply.github.com>
2025-05-23 16:49:54 -0400

    Update select_host_mode.py

--- commands/select_host_mode.py
+++ commands/select_host_mode.py
@@ -46,7 +46,7 @@ async def select_host_mode(interaction: discord.Interaction, option: str):
     ongoing["ban_mode"] = "Final"
     ongoing["firstban"] = False
     embed_msg_id = ongoing.get("embed_message_id")  
-    await update_ban_mode_choice_embed(interaction.channel, embed_msg_id, "Final")
+    
     await state.save_state(channel_id)
     
     if option == "Host":
@@ -55,10 +55,12 @@ async def select_host_mode(interaction: discord.Interaction, option: str):
         await state.save_state(channel_id)
         new_turn = await flip_turn(channel_id)
         await update_current_turn_embed(interaction.channel, embed_msg_id, new_turn) 
+        await update_ban_mode_choice_embed(interaction.channel, embed_msg_id, "Final")
         await state.save_state(channel_id)
     
     else:   
         await update_host_mode_choice_embed(interaction.channel,ongoing["embed_message_id"],option)         
+        await update_ban_mode_choice_embed(interaction.channel, embed_msg_id, "Final")
         await state.save_state(channel_id)
     
     await interaction.response.send_message(f"Option '{option}' recorded.",ephemeral=True)  
